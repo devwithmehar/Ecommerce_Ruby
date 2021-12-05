2d0cf13573bae265b7067a61f9a60aedef5f943d
Sahil Mehar <meharsahil207@gmail.com>
2021-12-05 17:11:23 -0600

    method are used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user! , :initialize_session
+  helper_method :cart
+
+  def initialize_session
+    session[:shopping_cart] ||= [] # empty array
+  end
+
+  def cart
+     Item.find(session[:shopping_cart] )
+  end
 end
